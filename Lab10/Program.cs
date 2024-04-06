94666746c607eb68b51a0f56118107792e2101db
Gabriel Windows <alvesgabriel1994@gmail.com>
2024-04-06 15:54:54 -0600

    feat: add method to calculate sum of numbers

--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Lab10;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Addition");
+        Console.WriteLine("Enter number n:");
+        int n = int.Parse(Console.ReadLine()!);
+        Console.WriteLine("Enter number m:");
+        int m = int.Parse(Console.ReadLine()!);
+        Console.WriteLine(CalculateSumOfNumbersBetween(n, m));
+    }
+
+    /// <summary>
+    /// Calculates the sum of integers between two given integers 'n' and 'm', inclusive.
+    /// </summary>
+    /// <param name="n">The starting integer.</param>
+    /// <param name="m">The ending integer.</param>
+    /// <returns>The sum of integers between 'n' and 'm'.</returns>
+    public static int CalculateSumOfNumbersBetween(int n, int m) => n == m ? n : n + CalculateSumOfNumbersBetween(n + 1, m);
+}
--- Lab10Tests/MainTests.cs
+++ Lab10Tests/MainTests.cs
@@ -2,4 +2,17 @@ namespace Lab10.Tests;
 
 public class ProgramTests
 {
+    [TestCase(1, 10, ExpectedResult = 55)]
+    [TestCase(5, 15, ExpectedResult = 110)]
+    [TestCase(-5, 5, ExpectedResult = 0)]
+    [TestCase(0, 0, ExpectedResult = 0)]
+    [TestCase(-10, -1, ExpectedResult = -55)]
+    public int CalculateSumOfNumbersBetween_ReturnsCorrectSum(int n, int m)
+    {
+        // Act
+        int result = Program.CalculateSumOfNumbersBetween(n, m);
+
+        // Assert
+        return result;
+    }
 }
